b8f02f837e694e7cc151c7d7d6b58635646f998d
Ben Morris <bensan.morris@googlemail.com>
2019-11-29 07:54:01 +0000

    Updated

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # cpp
 
-- copy_and_swap_idiom - based on https://stackoverflow.com/questions/3279543/what-is-the-copy-and-swap-idiom
+- copy_and_swap_idiom based on:
+
+    https://stackoverflow.com/questions/3279543/what-is-the-copy-and-swap-idiom
+    https://web.archive.org/web/20140113221447/http://cpp-next.com/archive/2009/08/want-speed-pass-by-value/
+    http://scottmeyers.blogspot.com/2014/06/the-drawbacks-of-implementing-move.html?m=1
--- copy_and_swap_idiom.cpp
+++ copy_and_swap_idiom.cpp
@@ -20,6 +20,11 @@ public:
         std::copy(rhs.data_, rhs.data_+size_, data_);
     }
 
+    Foo(Foo&& rhs) : Foo()
+    {
+        swap(*this, rhs);
+    }
+
     ~Foo()
     {
         delete [] data_;
@@ -34,7 +39,7 @@ public:
     Foo& operator = (Foo rhs) // nb. Foo passed in by value (creates copy)
     {
         swap(*this, rhs);     // this's data copied into rhs, rhs's data copied into this
-        return *this;         // when this function returns rhs is destroyed which contains this's old data
+        return *this;         // when this function return's rhs is destroyed which contains our old data
     }
 
 private:
@@ -42,10 +47,23 @@ private:
     T*     data_;
 };
 
+template<class T>
+Foo<T> make_foo()
+{
+    Foo<T> foo(5);
+    return foo;
+}
+
 int main()
 {
+    Foo<int> t = make_foo<int>();   // RVO (aka copy 'elision' i.e. copy 'edited out')
+    Foo<int> x(Foo<int>(3));
     Foo<int> a(10);
     Foo<int> b(20);
-    a = b;
+    a = b;                          // Assigns a to be a deep copy of b
+    {
+        Foo<int> a(3);
+        Foo<int> b = std::move(a);  // Moves a into b swapping a's old data (i.e. size=0 and data=nullptr into a so a is reset by the move as expected)
+    }
     return 0;
 }
